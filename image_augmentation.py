ab6bf70f4704a1159e4954b4105f81ed96a4be25
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2020-01-19 15:52:14 +0100

    feat: first version that writes images and labels

--- image_augmentation.py
+++ image_augmentation.py
@@ -7,8 +7,12 @@ from resizing_images import card_height, card_width
 from process_image import display_image_and_wait
 import numpy as np
 from draw_convex_hulls import get_convex_hull
-from draw_rectangles import upper_left_rectangle, upper_right_rectangle, bottom_left_rectangle, bottom_right_rectangle
+from draw_rectangles import upper_left_rectangle, upper_right_rectangle
+from draw_rectangles import bottom_left_rectangle, bottom_right_rectangle
 import os
+from functools import reduce
+import random
+import string
 
 generated_image_height = 720
 generated_image_width = 720
@@ -28,7 +32,7 @@ def get_keypoints(convex_hull, center_x=center_x, center_y=center_y):
     return key_points
 
 
-def get_boundingbox(keypoints, padding=3, label=None):
+def get_boundingbox(keypoints, padding=2, label=""):
     kpsx = [kp.x for kp in keypoints.keypoints]
     minx = max(0, int(min(kpsx) - padding))
     maxx = min(generated_image_width, int(max(kpsx) + padding))
@@ -95,18 +99,96 @@ def get_paths_and_names(folder_path):
     return image_paths
 
 
-def main():
-    image = cv2.imread("sarme.jpg")
-    image = image[:generated_image_width, :generated_image_height]
+def prepare_background(image):
+    resized_image = cv2.resize(image,
+                               dsize=(720, 720),
+                               interpolation=cv2.INTER_CUBIC)
 
+    return resized_image
+
+
+def all_files_from_folder(folder_path):
+    file_paths = os.listdir(folder_path)
+    file_paths = map(lambda f: os.path.join(folder_path, f), file_paths)
+
+    return list(file_paths)
+
+
+def get_random_background():
+    dtd_folder = "dtd/images/"
+    subfolders = os.listdir(dtd_folder)
+    subfolders = map(lambda f: os.path.join(dtd_folder, f), subfolders)
+    all_files = map(all_files_from_folder, subfolders)
+    all_files = reduce(lambda c, a: c + a, all_files, [])
+    all_files = list(all_files)
+    length = len(all_files)
+    index = random.randint(0, length)
+    file_path = all_files[index]
+    image = cv2.imread(file_path)
+
+    return image
+
+
+def get_classes():
+    file_path = "obj.names"
+    with open(file_path, 'r') as file:
+        data = file.read().split('\n')
+        dictionary = {data[i]: i for i in range(0, len(data))}
+
+        return dictionary
+
+
+def random_string(stringLength=10):
+    letters = string.ascii_lowercase
+
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+def get_formated_line(bbx):
+    text = "{class_number} {cx} {cy} {width}, {height}".format(
+        class_number=bbx.label,
+        cx=bbx.center_x,
+        cy=bbx.center_y,
+        width=bbx.width,
+        height=bbx.height)
+
+    return text
+
+
+def write_to_txt_file(file_name, bounding_boxes):
+    path = "result_images/"
+    path = os.path.join(path, file_name + ".txt")
+    with open(path, 'w') as text_file:
+        for bbx in bounding_boxes:
+            text = get_formated_line(bbx)
+            text_file.write(text)
+            text_file.write("\n")
+
+    return
+
+
+def write_image(file_name, image):
+    path = "result_images/"
+    path = os.path.join(path, file_name + ".jpg")
+    cv2.imwrite(path, image)
+
+    return
+
+
+def main():
     folder_path = "resized_images_with_alphachannel/"
     image_paths = get_paths_and_names(folder_path)
+    classes = get_classes()
 
     for (image_path, file_name) in image_paths:
+        image = get_random_background()
+        image = prepare_background(image)
+
         card_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
         empty_image = centered_card(card_image)
-        bounding_boxes = get_bounding_boxes_on_image(card_image, image,
-                                                     file_name)
+        label = classes[file_name.replace('.png', '')]
+        bounding_boxes_on_image = get_bounding_boxes_on_image(
+            card_image, image, label)
 
         seq = iaa.Sequential([
             iaa.Affine(translate_percent={
@@ -117,7 +199,10 @@ def main():
                        scale=(0.3, 1)),
         ])
         image_aug, bbs_aug = seq(image=empty_image,
-                                 bounding_boxes=bounding_boxes)
+                                 bounding_boxes=bounding_boxes_on_image)
+
+        random_file_name = random_string()
+        write_to_txt_file(random_file_name, bbs_aug.bounding_boxes)
 
         _, _, _, alphachannel = cv2.split(image_aug)
         alphachannel = 255 - alphachannel
@@ -126,9 +211,10 @@ def main():
         result = cv2.bitwise_or(result, image_aug)
 
         result = cv2.cvtColor(result, cv2.COLOR_BGRA2BGR)
-        result = bbs_aug.draw_on_image(result, size=2)
+        write_image(random_file_name, result)
 
-        display_image_and_wait(result)
+        # image_to_display = bbs_aug.draw_on_image(result, size=1)
+        # display_image_and_wait(image_to_display)
 
 
 if __name__ == '__main__':
